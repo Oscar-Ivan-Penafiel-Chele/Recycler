3bb5d549915de5844e3af92b2f0847b14f5c6252
Bryan Leon <bryanalc_1999@hotmail.com>
2021-08-06 01:05:13 -0500

    Registro 06-08-2021

--- app/src/main/java/com/example/recycler_project/register.java
+++ app/src/main/java/com/example/recycler_project/register.java
@@ -4,18 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 
 public class register extends AppCompatActivity {
 
+    EditText nombre1, apellido1, telefono1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_view);
-    }
+        nombre1=(EditText)findViewById(R.id.name_input);
+        apellido1=(EditText)findViewById(R.id.lastname_input);
+        telefono1=(EditText)findViewById(R.id.phone_input);
 
+
+    }
     public void next(View v){
+        String nombre, apellido, telefono;
+        nombre = nombre1.getText().toString();
+        apellido = apellido1.getText().toString();
+        telefono = telefono1.getText().toString();
+        if(nombre.equals("") || apellido.equals("")||telefono.equals("")){
+            Toast.makeText(this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
         Intent next = new Intent(this, register_2.class);
+        next.putExtra("nombre",nombre);
+        next.putExtra("apellido",apellido);
+        next.putExtra("telenofo",telefono);
         startActivity(next);
     }
 }
--- app/src/main/java/com/example/recycler_project/register_2.java
+++ app/src/main/java/com/example/recycler_project/register_2.java
@@ -5,17 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class register_2 extends AppCompatActivity {
-
+    EditText correo1, contraseña1;
+    String nombre1, apellido1, telefono1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_view_2);
+        correo1=(EditText)findViewById(R.id.username_input);
+        contraseña1=(EditText)findViewById(R.id.pass);
+        nombre1 = getIntent().getStringExtra("nombre");
+        apellido1 = getIntent().getStringExtra("apellido");
+        telefono1 = getIntent().getStringExtra("telefono");
+    }
+    public void registrar(View view){
+        String correo, contraseña;
+        correo=correo1.getText().toString();
+        contraseña = contraseña1.getText().toString();
+        if(correo.equals("") || contraseña.equals("")){
+            Toast.makeText(this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String URL="http://localhost:8080/Sentencias/Registro2.php?nombre="+nombre1+"&apellido="+apellido1+"&telefono="+telefono1+"&correo="+correo+"&contraseña="+contraseña;
+        RequestQueue servicio= Volley.newRequestQueue(this);
+        StringRequest respuesta = new StringRequest(
+                Request.Method.GET, URL, (response) -> {
+            Toast.makeText(getApplicationContext(),response, Toast.LENGTH_SHORT).show();
+            Intent next = new Intent(this, login.class);
+            startActivity(next);
+        }, (error) -> {
+            Toast.makeText(getApplicationContext(),"error comunicacion", Toast.LENGTH_SHORT).show();
+        });
+        servicio.add(respuesta);
+
+
     }
 
-    public void next(View v){
-        Intent next = new Intent(this, login.class);
-        startActivity(next);
-    }
 }
