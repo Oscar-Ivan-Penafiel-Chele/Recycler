4470f57029400ce689ef1362d271253e6f7c09c4
Oscar-Ivan-Penafiel-Chele <76529124+Oscar-Ivan-Penafiel-Chele@users.noreply.github.com>
2021-08-31 23:11:58 -0500

    EditProductFragment tiene que mejorarse

--- app/src/main/java/com/example/recycler_project/fragments/EditProductFragment.java
+++ app/src/main/java/com/example/recycler_project/fragments/EditProductFragment.java
@@ -1,12 +1,6 @@
 package com.example.recycler_project.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.recycler_project.HttpsTrustManager;
 import com.example.recycler_project.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +28,11 @@ import com.example.recycler_project.R;
  */
 public class EditProductFragment extends Fragment {
 
-    Button  btn_save, btn_back;
-    EditText txt_descrp, txt_peso;
+    private Button  btn_save, btn_back;
+    private EditText txt_descrp, txt_peso;
+    private RequestQueue requesQueue;
+    private double pesoxKilo,total, pesoR;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -69,10 +77,11 @@ public class EditProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_edit_product, container, false);
-        btn_back = root.findViewById(R.id.btn_back_edit_products);
-        btn_save = root.findViewById(R.id.btn_save_edit_product);
-        txt_descrp = root.findViewById(R.id.txt_descr_pro);
-        txt_peso = root.findViewById(R.id.txt_peso);
+
+        btn_back = root.findViewById(R.id.backEditProducts);
+        btn_save = root.findViewById(R.id.productEditButton);
+        txt_descrp = root.findViewById(R.id.productEdit);
+        txt_peso = root.findViewById(R.id.weightEdit);
 
         btn_back.setOnClickListener(this::onClick);
         btn_save.setOnClickListener(this::onClick);
@@ -82,11 +91,11 @@ public class EditProductFragment extends Fragment {
 
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_back_edit_products:
+            case R.id.backEditProducts:
                 onBackPress();
                 break;
-            case R.id.btn_save_edit_product:
-                Toast.makeText(getContext(), "Editado con éxito", Toast.LENGTH_SHORT).show();
+            case R.id.productEditButton:
+                //editProducts();
                 showSelectedFragment(new ProductsFragment());
                 break;
         }
@@ -104,5 +113,85 @@ public class EditProductFragment extends Fragment {
                     .addToBackStack(null)
                     .commit();
         }
+    }
+
+    private void getProductById(int id){
+        HttpsTrustManager.allowAllSSL();
+        final int[] idMaterial = new int[1];
+        String url= "https://192.168.1.12/Sentencias/Consulta.php?idProduct="+id;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, (response)->{
+            JSONObject jsonObject = null;
+            for(int i=0; i < response.length();i++){
+                try {
+                    jsonObject = response.getJSONObject(i);
+                    txt_descrp.setText(jsonObject.getString("descripcion_reciclaje"));
+                    txt_peso.setText(jsonObject.getString("peso_kilogramo"));
+                    idMaterial[0] = jsonObject.getInt("material_id");
+                    getPriceForMaterial(idMaterial[0]);
+                }catch (JSONException e){
+                    Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, (error)->{
+            Toast.makeText(getActivity().getApplicationContext(), "Error de Conexión", Toast.LENGTH_SHORT).show();
+        }
+        );
+        requesQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requesQueue.add(jsonArrayRequest);
+    }
+
+    private void editProducts(){
+        HttpsTrustManager.allowAllSSL();
+        int idProduct = 0 ; // Seteat el id para hacer el edit
+        String descripcion = txt_descrp.getText().toString();
+        double peso = Double.parseDouble(txt_peso.getText().toString());
+
+        if(descripcion.isEmpty() || txt_peso.getText().toString().isEmpty()){
+            Toast.makeText(getContext(),"Llenar todos los campos",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String url="https://192.168.1.12/Sentencias/updateUser.php?idProduct="+idProduct+"&descripcion="+descripcion+"&peso="+txt_peso.getText().toString();
+
+
+        Toast.makeText(getContext(), "Editado producto con éxito", Toast.LENGTH_SHORT).show();
+    }
+
+    public void getPriceForMaterial(int idMaterial){
+        double pesoM;
+
+        pesoM = Double.parseDouble(txt_peso.getText().toString());
+
+        HttpsTrustManager.allowAllSSL();
+
+        String url ="https://192.168.1.12/Sentencias/Consulta_Material.php?idMaterial="+idMaterial;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, (response)->{
+            JSONObject jsonObject = null;
+            for(int i=0; i<response.length();i++){
+                try {
+                    jsonObject = response.getJSONObject(i);
+                    pesoxKilo = Double.parseDouble(jsonObject.getString("precio_x_kilo"));
+                    total = calculationPriceTotal(pesoxKilo, pesoM);
+                    pesoR= Double.parseDouble(txt_peso.getText().toString());
+//                    SharedPreferences preferences = this.getActivity().getSharedPreferences("preferenciaslogin", Context.MODE_PRIVATE);
+//                    registerRecycling(preferences.getInt("id",0), id, pesoR, total);
+
+                }catch (JSONException e){
+                    Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    System.out.println(e.getMessage());
+                }
+            }
+        }, (error)->{
+            Toast.makeText(getActivity().getApplicationContext(), "error de Conexion", Toast.LENGTH_SHORT).show();
+        }
+        );
+        RequestQueue requesQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requesQueue.add(jsonArrayRequest);
+    }
+
+    public double calculationPriceTotal(double pesoxKilo, double precio){
+        double precioTotal=0;
+        precioTotal=pesoxKilo*precio;
+        return precioTotal;
     }
 }
